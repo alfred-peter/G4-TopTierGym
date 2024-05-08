a5f21e74d5f60dd96f7a87dbd7a953def27db11e
waramavictor <victorkinyua16@gmail.com>
2024-05-08 15:42:20 +0300

    victor's work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Plans from "./components/Plans/Plans";
+import Testimonials from "./components/Testimonials/Testimonials";
 
 function App() {
   return (
     <div className="App">
-     
+     <Plans />
+     <Testimonials />
     
     </div>
   );
