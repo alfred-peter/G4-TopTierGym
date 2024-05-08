b3170bba053724adafd1cb54863e8d9d29a758af
alvinKyle <alvinkyle70@gmail.com>
2024-05-08 18:00:31 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import './App.css';
-import Services from './components/Services';
-import WhyUs from './components/WhyUs';
+
 
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-     <Services/>
-     <WhyUs/>
-=======
 
->>>>>>> refs/remotes/origin/master
-    
     </div>
   );
 }
